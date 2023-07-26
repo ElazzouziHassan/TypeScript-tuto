c67718c07254448f5839cc81d2853ae75fb3455f
wizardy <wizardhassan99@gmail.com>
2023-07-26 21:18:43 +0100

    first functions

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,12 @@
-console.log('hello, typescript');
+function afficherInfo(nom: string, prenom: string, age: number): string
+{
+  return `Hello, it's ${nom } ${prenom} & I'm ${age} yo.`
+}
+
+// console.log(afficherInfo("El azzouzi", "Hassan", 26))
+
+function calculate2Numbers(a: number, b: number): number
+{
+  return a + b 
+}
+console.log(calculate2Numbers(5,9))
