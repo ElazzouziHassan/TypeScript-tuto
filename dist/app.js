4f3ee5e0b0db92adc51ed03ab52a83366d1cecf8
wizardy <wizardhassan99@gmail.com>
2023-07-26 15:26:32 +0100

    typing

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,19 @@
 var conpteur = document.querySelector("#compteur");
 var i = 0;
 var increment = function (e) {
+    e.preventDefault();
     i++;
-    conpteur.querySelector('span').innerText = i.toString();
+    var span = conpteur === null || conpteur === void 0 ? void 0 : conpteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+    }
 };
+function printId(id) {
+    if (typeof id === "number") {
+        console.log((id * 3).toString());
+    }
+    else {
+        console.log(id.toUpperCase());
+    }
+}
 conpteur.addEventListener('click', increment);
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,23 @@
 const conpteur = document.querySelector("#compteur");
 let i = 0;
 
-const increment = (e) => {
+const increment = (e: Event) => {
+  e.preventDefault();
   i++;
-  conpteur.querySelector('span').innerText = i.toString();
+  const span = conpteur?.querySelector('span');
+  if (span) {
+    span.innerText = i.toString();
+  }
 };
+
+function printId(id: string | number)
+{
+  if (typeof id === "number") {
+    console.log((id * 3).toString())
+  } else {
+    console.log(id.toUpperCase())
+  }  
+}
+
 
 conpteur.addEventListener('click',increment)
