59efa92def6b26b086b942edeb9cfab07593ef9e
wizardy <wizardhassan99@gmail.com>
2023-08-07 20:43:19 +0100

    function signatures

---
+++ src/FunctionSignatures.tsx
@@ -0,0 +1,11 @@
+let sayHello: (text: string) => void;
+
+sayHello = (text: string) => {
+  console.log(`${text}, World!`);
+}
+
+
+let addOperation: (numberOne: number, numberTwo: number) => void;
+addOperation = (a: number, b: number) => {
+  return a + b;
+}
