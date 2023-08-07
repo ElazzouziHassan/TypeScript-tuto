0c345e5013b89e9784e65f184b31dab92341aaec
wizardy <wizardhassan99@gmail.com>
2023-08-07 20:43:30 +0100

    compiled files

---
+++ public/dist/FunctionSignatures.js
@@ -0,0 +1,9 @@
+"use strict";
+let sayHello;
+sayHello = (text) => {
+    console.log(`${text}, World!`);
+};
+let addOperation;
+addOperation = (a, b) => {
+    return a + b;
+};
---
+++ public/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+function afficherInfo(nom, prenom, age) {
+    return `Hello, it's ${nom} ${prenom} & I'm ${age} yo.`;
+}
+// console.log(afficherInfo("El azzouzi", "Hassan", 26))
+function calculate2Numbers(a, b) {
+    return a + b;
+}
+console.log(calculate2Numbers(5, 9));
