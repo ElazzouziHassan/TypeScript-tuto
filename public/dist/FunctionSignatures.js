073673f42fc67058e1df0278b0226031eed997c5
wizardy <wizardhassan99@gmail.com>
2023-08-07 20:48:35 +0100

    new changes

--- public/dist/FunctionSignatures.js
+++ public/dist/FunctionSignatures.js
@@ -4,6 +4,16 @@ sayHello = (text) => {
     console.log(`${text}, World!`);
 };
 let addOperation;
-addOperation = (a, b) => {
-    return a + b;
+addOperation = (a, b, action) => {
+    if (action === 'add')
+        return a + b;
+    if (action === 'soust')
+        return a - b;
+    if (action === 'mult')
+        return a * b;
+    if (action === 'divise')
+        return a / b;
+    else
+        return 'invalide operation';
 };
+console.log(addOperation(2, 4, 'add'));
--- src/FunctionSignatures.tsx
+++ src/FunctionSignatures.tsx
@@ -5,7 +5,14 @@ sayHello = (text: string) => {
 }
 
 
-let addOperation: (numberOne: number, numberTwo: number) => void;
-addOperation = (a: number, b: number) => {
-  return a + b;
+let addOperation: (numberOne: number, numberTwo: number, action: string) => void;
+
+addOperation = (a: number, b: number, action: string) => {
+  if (action === 'add') return a + b;
+  if (action === 'soust') return a - b;
+  if (action === 'mult') return a * b;
+  if (action === 'divise') return a / b;
+  else return 'invalide operation';
 }
+
+console.log(addOperation(2,4,'add'))
