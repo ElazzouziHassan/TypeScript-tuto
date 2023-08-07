22c07a713602e5494c24d1f0e9e45b4d759edee6
wizardy <wizardhassan99@gmail.com>
2023-08-07 20:42:38 +0100

    remove

--- public/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function afficherInfo(nom, prenom, age) {
-    return `Hello, it's ${nom} ${prenom} & I'm ${age} yo.`;
-}
-// console.log(afficherInfo("El azzouzi", "Hassan", 26))
-function calculate2Numbers(a, b) {
-    return a + b;
-}
-console.log(calculate2Numbers(5, 9));
